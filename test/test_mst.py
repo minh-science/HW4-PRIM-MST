891754fa93cacba5c41e255ab32440266e4c975f
minh-science <156048292+minh-science@users.noreply.github.com>
2024-02-07 20:47:16 -0800

    DONT ADD HEAPQ TO REQUIREMENTS EITHER

--- test/test_mst.py
+++ test/test_mst.py
@@ -5,40 +5,40 @@ from mst import Graph
 from sklearn.metrics import pairwise_distances
 
 
-# def check_mst(adj_mat: np.ndarray, 
-#               mst: np.ndarray, 
-#               expected_weight: int, 
-#               allowed_error: float = 0.0001):
-#     """
+def check_mst(adj_mat: np.ndarray, 
+              mst: np.ndarray, 
+              expected_weight: int, 
+              allowed_error: float = 0.0001):
+    """
     
-#     Helper function to check the correctness of the adjacency matrix encoding an MST.
-#     Note that because the MST of a graph is not guaranteed to be unique, we cannot 
-#     simply check for equality against a known MST of a graph. 
+    Helper function to check the correctness of the adjacency matrix encoding an MST.
+    Note that because the MST of a graph is not guaranteed to be unique, we cannot 
+    simply check for equality against a known MST of a graph. 
 
-#     Arguments:
-#         adj_mat: adjacency matrix of full graph
-#         mst: adjacency matrix of proposed minimum spanning tree
-#         expected_weight: weight of the minimum spanning tree of the full graph
-#         allowed_error: allowed difference between proposed MST weight and `expected_weight`
+    Arguments:
+        adj_mat: adjacency matrix of full graph
+        mst: adjacency matrix of proposed minimum spanning tree
+        expected_weight: weight of the minimum spanning tree of the full graph
+        allowed_error: allowed difference between proposed MST weight and `expected_weight`
 
-#     TODO: Add additional assertions to ensure the correctness of your MST implementation. For
-#     example, how many edges should a minimum spanning tree have? Are minimum spanning trees
-#     always connected? What else can you think of?
+    TODO: Add additional assertions to ensure the correctness of your MST implementation. For
+    example, how many edges should a minimum spanning tree have? Are minimum spanning trees
+    always connected? What else can you think of?
 
-#     """
+    """
 
-#     def approx_equal(a, b):
-#         return abs(a - b) < allowed_error
+    def approx_equal(a, b):
+        return abs(a - b) < allowed_error
 
-#     total = 0
-#     for i in range(mst.shape[0]):
-#         for j in range(i+1):
-#             total += mst[i, j]
-#     # start edit
-#     # print("this is the mst \n", mst)
-#     print("TOTAL:", total)
-#     # end edit
-#     assert approx_equal(total, expected_weight), 'Proposed MST has incorrect expected weight'
+    total = 0
+    for i in range(mst.shape[0]):
+        for j in range(i+1):
+            total += mst[i, j]
+    # start edit
+    # print("this is the mst \n", mst)
+    print("TOTAL:", total)
+    # end edit
+    assert approx_equal(total, expected_weight), 'Proposed MST has incorrect expected weight'
 
 
 
@@ -81,43 +81,3 @@ def test_mst_student():
     
     """
     pass
-
-
-
-
-
-def check_mst(adj_mat: np.ndarray, 
-              mst: np.ndarray, 
-              expected_weight: int, 
-              allowed_error: float = 0.0001):
-    """
-    
-    Helper function to check the correctness of the adjacency matrix encoding an MST.
-    Note that because the MST of a graph is not guaranteed to be unique, we cannot 
-    simply check for equality against a known MST of a graph. 
-
-    Arguments:
-        adj_mat: adjacency matrix of full graph
-        mst: adjacency matrix of proposed minimum spanning tree
-        expected_weight: weight of the minimum spanning tree of the full graph
-        allowed_error: allowed difference between proposed MST weight and `expected_weight`
-
-    TODO: Add additional assertions to ensure the correctness of your MST implementation. For
-    example, how many edges should a minimum spanning tree have? Are minimum spanning trees
-    always connected? What else can you think of?
-
-    """
-
-    def approx_equal(a, b):
-        return abs(a - b) < allowed_error
-
-    total = 0
-    for i in range(mst.shape[0]):
-        for j in range(i+1):
-            total += mst[i, j]
-    # start edit
-    # print("this is the mst \n", mst)
-    print("TOTAL:", total)
-    # end edit
-    assert approx_equal(total, expected_weight), 'Proposed MST has incorrect expected weight'
-
